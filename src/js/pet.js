cd54ee9bd7b7afc5890dd2d8e4968852cb098135
Eva Wiedmann and Liam Kenna <eva+liam@>
2019-11-18 11:57:30 -0800

    adds pet methods and initial properties
    Co-Authored-By: Eva Wiedmann and Liam Kenna <eva@>

--- src/js/pet.js
+++ src/js/pet.js
@@ -3,5 +3,58 @@ export class Pet {
     this.petType = type;
     this.age = 0;
     this.level = 0;
+    this.form = 'baby';
+    this.health = 100;
+    this.hunger = 50;
+    this.hygiene = 50;
+    this.happiness = 50;
+    this.skills = [];
+    this.poop = 0;
+    this.alive = true;
+    this.wellness = 50;
+    this.sick = false;
   }
+
+  scoop() {
+    this.poop--;
+  }
+
+  feed() {
+    this.hunger += 10;
+  }
+
+  formChangeCheck() {
+    if (this.age + this.level >= 66) {
+      this.form = "old";
+    } else if (this.age + this.level >= 33) {
+      this.form = "mid";
+    }
+  }
+
+  isAlive() {
+    if (this.health < 1) this.alive = false;
+  }
+
+  isSick() {
+    if(this.poop > 7 || this.wellness < 10) this.sick = true;
+  }
+
+  exercise() {
+    this.health++;
+    this.happiness += 5;
+    this.wellness += 10;
+    this.hunger -= 5;
+    this.hygiene -= 5;
+  }
+
+  vet() {
+    this.health += 5;
+    this.happiness -= 10;
+    this.wellness += 15;
+  }
+
+  snuggle() {
+    this.happiness++;
+  }
+
 }
