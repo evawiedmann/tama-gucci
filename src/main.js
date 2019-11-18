29e13ec5a3cc0b11f02f19b12a02a29f123317eb
Eva Wiedmann and Liam Kenna <eva+liam@>
2019-11-18 09:54:29 -0800

    commented out stuff and added updated names
    Co-Authored-By: Eva Wiedmann and Liam Kenna <eva@>

--- src/main.js
+++ src/main.js
@@ -5,27 +5,27 @@ import './css/bootstrap.min.css';
 import './css/styles.css';
 
 // MAIN LOGIC
-import { User } from './js/user.js';
+import { Pet } from './js/pet.js';
 
 // Images from https://space-facts.com/transparent-planet-pictures/
-import earth from './img/earth.png';
+// import earth from './img/earth.png';
 
 // AUDIO
-import sunMp3 from './mp3/sun.mp3';
-const sunSound = new Audio();
-sunSound.src = sunMp3;
+// import sunMp3 from './mp3/sun.mp3';
+// const sunSound = new Audio();
+// sunSound.src = sunMp3;
 
 
 // TEMPLATING
-import { buildPlanetInfo, buildSunInfo } from './js/templates.js';
+// import { buildPlanetInfo, buildSunInfo } from './js/templates.js';
 
 
 // USER INTERFACE
 $(document).ready(function(){
 
-  let user;
+  let pet;
 
 
-  $('.sun').append(`<img id="Sun" class"sun" src="${sun}" alt="sun">`);
+  // $('.sun').append(`<img id="Sun" class"sun" src="${sun}" alt="sun">`);
 
 });
